f782dcf5bc5f71ae722aa0841b7f879d28134436
wesleyzup <wesley.bonissoni@zup.com.br>
2021-02-12 23:55:19 -0300

    Parei na aula de Smart Cast

---
+++ src/main/kotlin/br.com.exemplos/SmartCast.kt
@@ -0,0 +1,25 @@
+interface Expressao
+
+
+class Numero(val valor: Int) : Expressao
+
+class Soma(val equerdo: Expressao, val direito: Expressao) : Expressao
+
+
+fun avaliacao(expressao: Expressao): Int {
+    if (expressao is Numero) {
+        return expressao.valor
+    }
+
+    if (expressao is Soma) {
+        return avaliacao(expressao.equerdo) + avaliacao(expressao.direito)
+    }
+
+    throw IllegalAccessException("Expressão desconhecida")
+}
+
+fun main() {
+    // ((1 + 2) + 4)
+    val resultado = avaliacao(Soma(Soma(Numero(1), Numero(2)), Numero(4)))
+
+}
